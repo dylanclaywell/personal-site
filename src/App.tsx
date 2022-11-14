2291ff5c390fea5bf15768d3a886127bc65a5f90
Dylan Claywell <djclaywell@gmail.com>
2022-11-13 22:57:08 -0600

    Changed the direction of the site

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react'
 
 import colors from './colors'
-import Logo from './components/Logo'
-import NavLink from './components/NavLink'
+import Feature from './component/Feature'
 
 function App() {
   // Set all color variables from the shared colors file
@@ -19,15 +18,39 @@ function App() {
   return (
     <>
       <header className="flex items-center flex-wrap">
-        <div>
-          <Logo />
-        </div>
-        <nav className="flex grow justify-center space-x-8">
-          <NavLink>Home</NavLink>
-          <NavLink>About</NavLink>
-        </nav>
+        <nav className="flex grow justify-center space-x-8"></nav>
       </header>
-      <main></main>
+      <main>
+        <div className="text-center h-screen flex flex-col justify-center">
+          <h1 className="text-[10vw]">Howdy! 👋</h1>
+          <p className="font-[Poppins]">
+            My name is <a href="#about-me">Dylan Claywell</a>
+          </p>
+        </div>
+        <Feature
+          id="about-me"
+          title="Who am I?"
+          imageUrl="https://via.placeholder.com/500"
+          paragraphs={[
+            `I'm a software engineer by trade, and a creator by passion. I
+            love making something new with my own two hands, from games to web
+            applications.`,
+            `I love React and Typescript, and I'm passionate about making
+            interfaces that are easy to use and look great in the process.`,
+          ]}
+        />
+        <Feature
+          id="my-work"
+          title="What am I working on?"
+          imageUrl="https://via.placeholder.com/500"
+          paragraphs={[
+            `This website is written in React 18 and Typescript, and is
+             styled using Tailwind CSS. It's hosted on Google Cloud Platform
+             using App Engine.
+            `,
+          ]}
+        />
+      </main>
     </>
   )
 }
---
+++ src/component/Feature.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+interface Props {
+  id: string
+  title: string
+  paragraphs: string[]
+  imageUrl: string
+}
+
+export default function Feature({ id, title, paragraphs, imageUrl }: Props) {
+  return (
+    <section
+      id={id}
+      className="flex flex-col md:flex-row p-4 space-y-2 md:space-y-0 md:space-x-2"
+    >
+      <div className="space-y-4 basis-1/2">
+        <h2 className="text-2xl font-bold">{title}</h2>
+        {paragraphs.map((paragraph, index) => (
+          <p key={index} className="basis-1/2 font-[Poppins]">
+            {paragraph}
+          </p>
+        ))}
+      </div>
+      <img className="basis-1/2 w-1/2 object-contain" src={imageUrl} />
+    </section>
+  )
+}
